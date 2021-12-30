21bda62d202b06c84a1993d585ab7185c2f827fb
Kishor1506 <96857766+Kishor1506@users.noreply.github.com>
2021-12-30 11:01:20 +0530

    added p tag

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -9,6 +9,7 @@ const SecondPage = () => (
     <Seo title="Page two" />
     <h1>Hi from the second page</h1>
     <p>Welcome to page 2</p>
+  <p>Since Gatsby is a static site generator at its core, let’s explore this concept before diving into anything else. If you come across a plain HTML page that has been sitting on a server since the mid 1990s, you’ve found a static website. So what, you ask, is particularly innovative about something that can generate static content on the web? </p>
     <Link to="/">Go back to the homepage</Link>
   </Layout>
 )
