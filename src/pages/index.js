f2f533b9f8cb8ffa3bd86fc9ea3aadbff80c7aca
Kishor1506 <96857766+Kishor1506@users.noreply.github.com>
2021-12-30 11:00:09 +0530

    added intro

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,6 +11,9 @@ const IndexPage = () => (
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
+  <p>You’re currently visiting a website powered by Gatsby, and there is a reason for that. Not long ago, we at mParticle decided to invest in improving the experience of our website. We wanted pages to load faster while adhering to the highest standards of accessibility. We wanted to put extensible tools in the hands of our developers to build and expand upon the site well into the future. We considered what we had, where we wanted to go, and the options we had for getting there. 
+
+In the end, we decided to overhaul our site with Gatsby, and we’re glad we did. To learn more specifics about our particular use case with Gatsby, here's a post detailing our website overhaul. Here, we'll explore the framework itself––what it is, what it’s built on, and what differentiates it from other tools in the world of static web applications. </p>
     <StaticImage
       src="../images/gatsby-astronaut.png"
       width={300}
